da2466673ac26d3f280f82c05f9199f46f906cee
Louloubiwan <147647358+Louloubiwan@users.noreply.github.com>
2025-12-27 11:03:30 +0100

    Interface graphique
    ajout une interface avec des option

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ import openpyxl
 
 import classes
 from classes import *
+
+
 # initialisation #
 screen_width= 1200
 screen_height = 900
@@ -27,14 +29,42 @@ File = openpyxl.load_workbook(tableau_excel)
 Sheet = File.active
 
 
+SimpleGraphics.resize(screen_width, screen_height)
+nuage = classes.PointCloud()
 
-def setup():
-    SimpleGraphics.resize(screen_width, screen_height)
-    nuage = classes.PointCloud()
-    nuage.createRandomPoints(1000,screen_width,screen_height)
-    nuage.drawAllPoints()
-    nuage.getPositions()
-    nuage.OptimizeCloud()
+def Input():
+
+    print("--------------------------------\n Séléctionner d'abord un paramètre Basique avant d'utiliser une méthode\n \n 1 - Basique : Afficher des points aléatoirement \n 2 - Basique : afficher les points du tableau \n \n 3 - Méthode : afficher la moyenne de ces points \n \n 10 - Système : Nettoyer les points \n 11 - Système : Quitter\n -------------------------------")
+    while True:
+            reponse = str(input("Séléctionner un paramètre : "))
+
+            if reponse == "1":
+                nombre_de_points = int(input("Combien de points voulez-vous afficher ?"))
+                nuage.createRandomPoints(nombre_de_points,screen_width,screen_height)
+                setup()
+
+            elif reponse == "2":
+                  nuage.GetPoints(Sheet)
+                  setup()
+                    
+            elif reponse == "3":
+                  Moyenne(nuage)
+                  setup()
+
+            elif reponse == "10":
+                  SimpleGraphics.clear()
+                 
+            elif reponse == "11":
+                  print("Fermeture du programme...")
+                  SimpleGraphics.close()
+                  exit()
+                  
+def setup():       
+        nuage.drawAllPoints()
+        nuage.getPositions()
+        nuage.OptimizeCloud()   
+
+
 if __name__ == "__main__":
         random.seed()
-        setup()
+        Input()
